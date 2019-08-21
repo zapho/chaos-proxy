40d0d000745a9adaf89c8253cc5f44695888c34e
Fabrice Aupert <fabrice.aupert@agfa.com>
2019-08-21 16:54:56 +0200

    adds basic testing for proxy

--- quarkus/src/test/java/com/agfa/orbis/orme/ChaosProxyResourceTest.java
+++ quarkus/src/test/java/com/agfa/orbis/orme/ChaosProxyResourceTest.java
@@ -1,21 +1,34 @@
 package com.agfa.orbis.orme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class PingResourceTest {
+public class ChaosProxyResourceTest {
+
+    private final RequestSpecification proxySpec = new RequestSpecBuilder().setProxy(8888).build();
 
     @Test
-    public void testHelloEndpoint() {
+    public void when_no_configuration_http_request_is_passed_through() {
         given()
-          .when().get("resources/ping")
-          .then()
-             .statusCode(200)
-             .body(is("pong"));
+                .log().all()
+                .when().delete("resources/chaos/conf")
+                .then()
+                .log().all()
+                .statusCode(200);
+
+        given()
+                .spec(proxySpec)
+                .log().all()
+                .when().get("http://wwww.google.com")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
 }
