0d589fa3540693eebd386e67d81218538ec1f5f3
Fabrice Aupert <fabrice.aupert@dedalus-group.com>
2021-03-30 09:57:41 +0200

    code cleanup

--- src/main/java/fr/zapho/chaosproxy/ProxyConfiguration.java
+++ src/main/java/fr/zapho/chaosproxy/ProxyConfiguration.java
@@ -1,14 +1,3 @@
-/*
- *                  C O P Y R I G H T  (c) 2014
- *     A G F A   H E A L T H C A R E   C O R P O R A T I O N
- *                     All Rights Reserved
- *
- *
- *         THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
- *                       AGFA CORPORATION
- *        The copyright notice above does not evidence any
- *       actual or intended publication of such source code.
- */
 package fr.zapho.chaosproxy;
 
 import javax.json.bind.annotation.JsonbProperty;
--- src/main/java/fr/zapho/chaosproxy/ProxyResource.java
+++ src/main/java/fr/zapho/chaosproxy/ProxyResource.java
@@ -1,20 +1,7 @@
-/*
- *                  C O P Y R I G H T  (c) 2014
- *     A G F A   H E A L T H C A R E   C O R P O R A T I O N
- *                     All Rights Reserved
- *
- *
- *         THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
- *                       AGFA CORPORATION
- *        The copyright notice above does not evidence any
- *       actual or intended publication of such source code.
- */
 package fr.zapho.chaosproxy;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
